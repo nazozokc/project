d456e2600dca1a9ea085237b65dfa43c7f114721
memo1 <kana.a0711@outlook.jp>
2026-01-19 19:02:07 +0900

    edit count-API

--- typescript/API/count-API/src/index.ts
+++ typescript/API/count-API/src/index.ts
@@ -2,3 +2,30 @@ import express, { Request, Response } from "express";
 
 const app = express();
 app.use(express());
+
+let count: number = 0;
+
+app.get("/api/count", (req: Request, res: Response) => {
+  res.json({ count });
+});
+
+app.post("/api/count/add", (req: Request, res: Response) => {
+  count++;
+  res.json({ count });
+});
+
+app.post("/api/count/minus", (req: Request, res: Response) => {
+  count--;
+  res.json({ count });
+});
+
+app.post("/api/count/reset", (req: Request, res: Response) => {
+  count = 0;
+  res.json({ count });
+});
+
+app.listen(3000, () => {
+  console.log("enable!!!!");
+});
+
+export { app };
