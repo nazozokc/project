dceb811a681113e45e8002184c0e072a276cc45f
nazozokc <nazozokc@gmail.com>
2026-01-26 21:19:35 +0900

    edit access-token-API/src/index.ts

--- typescript/API/access-token-API/src/index.ts
+++ typescript/API/access-token-API/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import fs from "fs";
+import fs, { existsSync } from "fs";
 import crypto from "crypto";
 
 const app = express();
@@ -8,7 +8,7 @@ app.use(express.json());
 type User = {
   id: number;
   username: string;
-  password: any;
+  password: string;
 };
 
 type AuthToken = {
@@ -16,3 +16,40 @@ type AuthToken = {
   userId: number;
   createdAt: string;
 };
+
+const FILE_PATH = "./tokens.json";
+
+const loadtoken = (): AuthToken => {
+  if (!fs.existsSync(FILE_PATH)) {
+    return {
+      token: "",
+      userId: 0,
+      createdAt: "",
+    }
+  };
+
+  const data = fs.readFileSync(FILE_PATH, "utf-8");
+
+  const parsed = JSON.parse(data);
+
+  return {
+    token: typeof parsed.token === "string" ? parsed.token : "",
+    userId: typeof parsed.userId === "number" ? parsed.userId : 0,
+    createdAt: typeof parsed.createdAt === "string" ? parsed.createdAt : "",
+  };
+}
+
+const saveLog = (log: AuthToken): void => {
+  fs.writeFileSync(
+    FILE_PATH,
+    JSON.stringify(log, null, 2),
+  );
+};
+
+let token: AuthToken = loadtoken();
+
+app.get("/api/token", (req: Request, res: Response) => {
+  res.json({ token });
+})
+
+app.post("/api/token", (req: Request, res: Response) => {})
