0e32595b3f6ef67793936711fc309b6f531bed3c
nazozokc <nazozokc@gmail.com>
2026-01-26 20:31:56 +0900

    edit index.ts

--- typescript/API/access-token-API/src/index.ts
+++ typescript/API/access-token-API/src/index.ts
@@ -4,3 +4,15 @@ import crypto from "crypto";
 
 const app = express();
 app.use(express.json());
+
+type User = {
+  id: number;
+  username: string;
+  password: any;
+};
+
+type AuthToken = {
+  token: string;
+  userId: number;
+  createdAt: string;
+};
