e8787dac0399f2f9b88660ef99a284e85487209e
nazozokc <nazozokc@gmail.com>
2026-02-04 15:46:51 +0900

    add 3.md

---
+++ typescript/API/API-with-API-key-authentication/src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, NextFunction } from "express";
+import fs from "fs";
+
+const app = express();
+app.use(express.json());
+
+type ApiKey = {
+  key: string;
+  createAt: string;
+};
+
+type ApiKeyStore = {
+  keys: ApiKey[];
+};
+
+const FILE_PATH = "./apikeys.json";
+
+const loadApiKeys = (): ApiKeyStore => {
+  const raw = fs.readFileSync(FILE_PATH, "utf-8");
+  return JSON.parse(raw) as ApiKeyStore;
+};
+
+declare global {
+  namespace Express {
+    apikey?: ApiKey
+  }
+}
